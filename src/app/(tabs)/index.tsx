2c4a4cbe3bd3b5b9d701c1bec637488427552220
furqonstudio <furqonstudio@gmail.com>
2025-03-20 15:08:49 +0700

    feat(home): create weather card

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Button from '@/components/atomics/Button'
@@ -10,7 +10,7 @@ import SortIcon from '@/assets/icons/SortIcon'
 
 const index = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="mx-[18px]">
       <View className="flex flex-row justify-between items-center">
         <View>
           <Text className="font-normal text-[12px] -mb-1">Good Morning</Text>
@@ -27,6 +27,26 @@ const index = () => {
           <Text className="text-xs">Your location ...</Text>
         </View>
         <SortIcon />
+      </View>
+      <View className="flex flex-row justify-between items-center bg-primary h-[129px] rounded-2xl px-8 py-3">
+        <View>
+          <Text className="text-xs font-normal text-white">This Day</Text>
+          <Text className="text-xs font-bold text-white">
+            Sunday, 13 December
+          </Text>
+          <Text className="text-[50px] font-bold text-white -my-2">23°C</Text>
+          <Text className="text-xs font-normal text-white">
+            Day 25°C, Night 25°C
+          </Text>
+        </View>
+        <View className="flex gap-2">
+          <Image
+            source={require('assets/images/weather.png')}
+            className="size-[70px] p-1"
+            resizeMode="contain"
+          />
+          <Text className="text-xs font-normal text-white">Thunderstorm</Text>
+        </View>
       </View>
       <CardList />
     </SafeAreaView>
